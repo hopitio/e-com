da6a038f0fc61a1cfa5d183d07c677a34ad60b7b
anlt <le.thanh.an@vsi-international.com>
2014-03-14 08:32:56 +0700

    Commit docs folder

--- docs/database.sql
+++ docs/database.sql
@@ -1,3 +1,13 @@
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+)
+
 CREATE TABLE t_user(
 	id	INT PRIMARY KEY AUTO_INCREMENT,
 firstname	VARCHAR(50),
