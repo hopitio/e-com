5eedf7f992a8eafa09643e7fdd395658bdb08cd6
Duong Tuan Anh <goat91@gmail.com>
2014-03-22 22:44:25 +0700

    a

--- src/application/controllers/home.php
+++ src/application/controllers/home.php
@@ -9,13 +9,11 @@ class home extends BaseController
 
     public function showHome()
     {
-        DB::getInstance()->debug = 1;
         $data['materials'] = ListMaterialMapper::make()->findAll();
         
         $data['categories'] = CategoryMapper::make()->filterLanguage("vi")->findAll();
         
         $data['products'] = ProductFixedMapper::make()->autoloadAttributes(true, "vi")->findAll();
-        var_dump($product = $data['products'][0]->getName()->getTrueValue());
         $this->load->model('Category');
         $cate = $this->Category->loadCategory();
         LayoutFactory::getLayout(LayoutFactory::TEMP_ONE_COl)->setCss(array("/style/homePage.css"))->setData($data)->render('home');
