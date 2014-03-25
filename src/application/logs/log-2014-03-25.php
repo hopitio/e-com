dcb08c962589c61bc86114b160185bcf28af1830
Duong Tuan Anh <goat91@gmail.com>
2014-03-25 20:28:48 +0700

    Fix language

---
+++ src/application/logs/log-2014-03-25.php
@@ -0,0 +1,73 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-25 20:16:45 --> exception 'LableKeyNotFound' with message 'Không tìm thấy key màn hình tương ứng 
+    
+    
+    
+    
+    
+->product/details' in C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\DefaultLoadLanguage.php:13
+Stack trace:
+#0 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(97): DefaultLoadLanguage->getScreen('product/details', 'VN-VI', Array)
+#1 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(154): MultilLanguageManager->getLangViaScreen('product/details', 'VN-VI')
+#2 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\layout\PortalOneColumnLayout.php(16): MultilLanguageManager->attachedLanguageDataToScreen('product/details', Array)
+#3 C:\Users\ta\Documents\GitHub\e-com\src\application\controllers\product.php(17): PortalOneColumnLayout->render('product/details')
+#4 [internal function]: product->details('1')
+#5 C:\Users\ta\Documents\GitHub\e-com\src\application\core\MY_Controller.php(75): call_user_func_array(Array, Array)
+#6 C:\Users\ta\Documents\GitHub\e-com\src\system\core\CodeIgniter.php(323): MY_Controller->_remap('details', Array)
+#7 C:\Users\ta\Documents\GitHub\e-com\src\docroot\index.php(258): require_once('C:\Users\ta\Doc...')
+#8 {main}
+ERROR - 2014-03-25 20:19:18 --> exception 'LableKeyNotFound' with message 'Không tìm thấy key màn hình tương ứng 
+    
+    
+    
+    
+    
+    
+->product/details' in C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\DefaultLoadLanguage.php:13
+Stack trace:
+#0 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(97): DefaultLoadLanguage->getScreen('product/details', 'VN-VI', Array)
+#1 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(154): MultilLanguageManager->getLangViaScreen('product/details', 'VN-VI')
+#2 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\layout\PortalOneColumnLayout.php(16): MultilLanguageManager->attachedLanguageDataToScreen('product/details', Array)
+#3 C:\Users\ta\Documents\GitHub\e-com\src\application\controllers\product.php(17): PortalOneColumnLayout->render('product/details')
+#4 [internal function]: product->details('1')
+#5 C:\Users\ta\Documents\GitHub\e-com\src\application\core\MY_Controller.php(75): call_user_func_array(Array, Array)
+#6 C:\Users\ta\Documents\GitHub\e-com\src\system\core\CodeIgniter.php(323): MY_Controller->_remap('details', Array)
+#7 C:\Users\ta\Documents\GitHub\e-com\src\docroot\index.php(258): require_once('C:\Users\ta\Doc...')
+#8 {main}
+ERROR - 2014-03-25 20:20:56 --> exception 'LableKeyNotFound' with message 'Không tìm thấy key màn hình tương ứng 
+    
+    
+    
+    
+    
+    
+->product/details' in C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\DefaultLoadLanguage.php:14
+Stack trace:
+#0 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(97): DefaultLoadLanguage->getScreen('product/details', 'VN-VI', Array)
+#1 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(154): MultilLanguageManager->getLangViaScreen('product/details', 'VN-VI')
+#2 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\layout\PortalOneColumnLayout.php(16): MultilLanguageManager->attachedLanguageDataToScreen('product/details', Array)
+#3 C:\Users\ta\Documents\GitHub\e-com\src\application\controllers\product.php(17): PortalOneColumnLayout->render('product/details')
+#4 [internal function]: product->details('1')
+#5 C:\Users\ta\Documents\GitHub\e-com\src\application\core\MY_Controller.php(75): call_user_func_array(Array, Array)
+#6 C:\Users\ta\Documents\GitHub\e-com\src\system\core\CodeIgniter.php(323): MY_Controller->_remap('details', Array)
+#7 C:\Users\ta\Documents\GitHub\e-com\src\docroot\index.php(258): require_once('C:\Users\ta\Doc...')
+#8 {main}
+ERROR - 2014-03-25 20:21:47 --> exception 'LableKeyNotFound' with message 'Không tìm thấy key màn hình tương ứng 
+    
+    
+    
+    
+    
+    
+->product/details' in C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\DefaultLoadLanguage.php:14
+Stack trace:
+#0 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(97): DefaultLoadLanguage->getScreen('product/details', 'VN-VI', Array)
+#1 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\multiLanguage\MultilLanguageManager.php(154): MultilLanguageManager->getLangViaScreen('product/details', 'VN-VI')
+#2 C:\Users\ta\Documents\GitHub\e-com\src\application\libraries\layout\PortalOneColumnLayout.php(16): MultilLanguageManager->attachedLanguageDataToScreen('product/details', Array)
+#3 C:\Users\ta\Documents\GitHub\e-com\src\application\controllers\product.php(17): PortalOneColumnLayout->render('product/details')
+#4 [internal function]: product->details('1')
+#5 C:\Users\ta\Documents\GitHub\e-com\src\application\core\MY_Controller.php(75): call_user_func_array(Array, Array)
+#6 C:\Users\ta\Documents\GitHub\e-com\src\system\core\CodeIgniter.php(323): MY_Controller->_remap('details', Array)
+#7 C:\Users\ta\Documents\GitHub\e-com\src\docroot\index.php(258): require_once('C:\Users\ta\Doc...')
+#8 {main}
