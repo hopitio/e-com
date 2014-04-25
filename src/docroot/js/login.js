31570ebbf6e0a709091ab7e0341adeffa8782784
ANLT <ANLT@PROBOOK450>
2014-04-24 20:55:06 -0700

    commit src

---
+++ src/docroot/js/login.js
@@ -0,0 +1,39 @@
+/*
+ * 
+ *Thực hiện các action javascript liên quan đến login page.
+ * CREATE BY : ANLT 
+ * CREATE DATE : 20140327
+ */
+
+$(document).ready(function(){
+    $('#likfacebooklogin').click(facebookAuthenicate);
+});
+
+function facebookAuthenicate()
+{
+    FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+            getApiMe();
+          return;
+        }
+        FB.login(function(loginResponse){
+            if (loginResponse.status === 'connected') {
+                getApiMe();
+                return;
+              }
+        },{scope:'email,user_birthday'});
+       });
+}
+
+function getApiMe(){
+    FB.api('/me',function(response){
+        $('#facebooklogin').find('input[name="postValue"]').val(JSON.stringify(response));
+        var frm = $('#facebooklogin');
+        frm.submit();
+        console.log(response);
+    });
+}
+
+
+
+
