29f7bf37f4a2077f2770b035c48843ab3dea7fcd
ANLT <ANLT@PROBOOK450>
2014-05-06 08:32:24 +0700

    commit controller

---
+++ src/application/controllers/testingController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Chứa các hàm liên quan đến testing
+ * @author ANLT
+ *
+ */
+class testingController extends BaseController{
+    function getMockupScreenList(){
+        LayoutFactory::getLayout(LayoutFactory::TEMP_ONE_COl)
+        ->setCss(
+            array('/style/list.css')
+        )
+        ->render('product/list');
+    }
+}
