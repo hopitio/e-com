4e762f388b3ccc4d0806f941729fb8de0549b933
ANLT <ANLT@PROBOOK450>
2014-05-16 23:41:32 +0700

    commit src

--- src/application/config/production/database.php
+++ src/application/config/production/database.php
@@ -48,12 +48,11 @@
 $active_group = 'lynx';
 $active_record = TRUE;
 
-$db['lynx']['hostname'] = '10.0.0.150';
-$db['lynx']['username'] = 'lynx';
-$db['lynx']['password'] = 'wii2009kdkCsjkK';
-$db['lynx']['database'] = 'lynx_131209';
-
-$db['lynx']['dbdriver'] = 'mysql';
+$db['lynx']['hostname'] = '127.0.0.1';
+$db['lynx']['username'] = 'root';
+$db['lynx']['password'] = 'root';
+$db['lynx']['database'] = 'eproject';
+$db['lynx']['dbdriver'] = 'mysqli';
 $db['lynx']['dbprefix'] = '';
 $db['lynx']['pconnect'] = TRUE;
 $db['lynx']['db_debug'] = TRUE;
@@ -65,6 +64,25 @@ $db['lynx']['swap_pre'] = '';
 $db['lynx']['autoinit'] = TRUE;
 $db['lynx']['stricton'] = FALSE;
 $db['lynx']['port']     = 3306;
+
+
+$db['portal']['hostname'] = '127.0.0.1';
+$db['portal']['username'] = 'root';
+$db['portal']['password'] = 'root';
+$db['portal']['database'] = 'portal_e_project';
+$db['portal']['dbdriver'] = 'mysql';
+$db['portal']['dbprefix'] = '';
+$db['portal']['pconnect'] = TRUE;
+$db['portal']['db_debug'] = TRUE;
+$db['portal']['cache_on'] = FALSE;
+$db['portal']['cachedir'] = '';
+$db['portal']['char_set'] = 'utf8';
+$db['portal']['dbcollat'] = 'utf8_general_ci';
+$db['portal']['swap_pre'] = '';
+$db['portal']['autoinit'] = TRUE;
+$db['portal']['stricton'] = FALSE;
+$db['portal']['port']     = 3306;
+
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
