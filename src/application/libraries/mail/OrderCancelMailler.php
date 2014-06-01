3ab2d22138d76342ac844983f3016ebef27e0e9b
ANLT <ANLT@PROBOOK450>
2014-06-01 11:25:57 +0700

    commit

---
+++ src/application/libraries/mail/OrderCancelMailler.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * class thực hiện việc chuyển mail Confirm
+ * @author ANLT
+ * @since 20140330
+ */
+class OrderCancelMailler extends AbstractStaff{
+    protected $config_key = 'OrderCancel';
+    
+    
+    /* (non-PHPdoc)
+     * @see AbstractStaff::sendMail()
+    */
+    protected function sendMail()
+    {
+        $fullName = $this->config[MAILLER_FULLNAME];
+        $email = $this->config[MAILLER_USER];
+        $this->CI->email->from($email, $fullName);
+        $this->CI->email->to($this->to);
+        $mailLanguage = MultilLanguageManager::getInstance()->getLangViaScreen('mail', $this->languageKey);
+        $this->CI->email->subject($mailLanguage->newpasswordNofication);
+        $msg = $this->buildMailContent();
+        $this->CI->email->message($msg);
+        $this->CI->email->send();
+    }
+    
+    /**
+     * AUTO BUILD.
+     */
+    private function buildMailContent()
+    {
+        $this->CI->load->helper('file');
+        $temp = $this->CI->config->item('temp_mail_folder');
+        $temp .= $this->languageKey.'/'. $this->config[MAILLER_TEMP];
+        $mailContent = read_file($temp);
+        
+        $order = $this->mailData['order'];
+        $name = '';
+        $order_number = $order->id;
+        $this->preOrderInformation($order, $name, $order_number);
+        $mailContent = str_replace('{name}',$name,$mailContent);
+        $mailContent = str_replace('{order_number}',$order_number,$mailContent);
+
+        return $mailContent;
+    }  
+
+    private function preOrderInformation($order,&$name,&$order_number){
+        foreach ($order->invoice->contacts as $contact){
+            if($this->to == $contact->email_contact){
+                $name = $contact->full_name;
+                break;
+            }
+        }
+    }
+}
---
+++ src/application/libraries/mail/SellerDeliveredMailler.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * class thực hiện việc chuyển mail Confirm
+ * @author ANLT
+ * @since 20140330
+ */
+class SellerDeliveredMailler extends AbstractStaff{
+    protected $config_key = 'SellerDelivered';
+    
+    
+    /* (non-PHPdoc)
+     * @see AbstractStaff::sendMail()
+    */
+    protected function sendMail()
+    {
+        $fullName = $this->config[MAILLER_FULLNAME];
+        $email = $this->config[MAILLER_USER];
+        $this->CI->email->from($email, $fullName);
+        $this->CI->email->to($this->to);
+        $mailLanguage = MultilLanguageManager::getInstance()->getLangViaScreen('mail', $this->languageKey);
+        $this->CI->email->subject($mailLanguage->newpasswordNofication);
+        $msg = $this->buildMailContent();
+        $this->CI->email->message($msg);
+        $this->CI->email->send();
+    }
+    
+    /**
+     * AUTO BUILD.
+     */
+    private function buildMailContent()
+    {
+        $this->CI->load->helper('file');
+        $temp = $this->CI->config->item('temp_mail_folder');
+        $temp .= $this->languageKey.'/'. $this->config[MAILLER_TEMP];
+        $mailContent = read_file($temp);
+        
+        $order = $this->mailData['order'];
+        
+        $seller_name = '';
+        $order_number = $order->id;
+        $buyer_name = '';
+        $buyer_contact = '';
+        $buyer_phone = '';
+        
+        $this->preOrderInformation($order, $order_number, $seller_name, $buyer_name, $buyer_contact, $buyer_phone);
+         
+        $mailContent = str_replace('{seller_name}',$seller_name,$mailContent);
+        $mailContent = str_replace('{order_number}',$order_number,$mailContent);
+        $mailContent = str_replace('{buyer_name}',$buyer_name,$mailContent);
+        $mailContent = str_replace('{buyer_contact}',$buyer_contact,$mailContent);
+        $mailContent = str_replace('{buyer_phone}',$buyer_phone,$mailContent);
+
+        return $mailContent;
+    }  
+
+    /**
+     * 
+     * @param unknown $order
+     * @param unknown $name
+     * @param unknown $order_number
+     */
+    private function preOrderInformation($order,&$order_number,&$seller_name,&$buyer_name,&$buyer_contact,&$buyer_phone){
+        foreach ($order->invoice->products as $product){
+            if($product->seller_email == $this->to){
+                $seller_name = $product->seller_name;
+                break;
+            }
+        }
+        
+        foreach ($order->invoice->shippings as $shipping)
+        {
+            if($shipping->shipping_type == DatabaseFixedValue::SHIPPING_TYPE_SHIP && $shipping->status == DatabaseFixedValue::SHIPPING_STATUS_ACTIVE){
+               $buyer_name = $shipping->contact->full_name;
+               $buyer_contact = "{$shipping->contact->street_address} , {$shipping->contact->city_district}, {$shipping->contact->state_province}";
+               $buyer_phone = $shipping->contact->telephone;
+               break;
+            }
+        }
+        
+    }
+}
