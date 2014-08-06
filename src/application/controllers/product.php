fc0b795ad510ce8aad2481311e76835dc5e2bac6
Duong Tuan Anh <goat91@gmail.com>
2014-08-07 00:19:25 +0700

    gg detail

--- src/application/controllers/product.php
+++ src/application/controllers/product.php
@@ -29,7 +29,6 @@ class product extends BaseController
         $parentCategory = CategoryMapper::make()->setLanguage($user->languageKey)->filterID($product->fkCategory)->find();
         $ancestors = explode('/', trim($parentCategory->path, '/'));
         $breadcrums = array();
-
         foreach ($ancestors as $cateID)
         {
             if ($cateID == $parentCategory->id)
@@ -44,7 +43,12 @@ class product extends BaseController
         $productName = strlen(strval($product->getName())) > 50 ? substr((string) $product->getName(), 0, 50) . '...' : (string) $product->getName();
         $breadcrums[$productName] = NULL;
 
-        $data = array('product' => $product);
+        $data = array(
+            'product'    => $product,
+            'breadcrums' => $breadcrums,
+            'ancestors'  => $ancestors
+        );
+        $data['secondLvlCates'] = CategoryMapper::make()->setLanguage($user->languageKey)->filterParent($ancestors[0])->findAll();
         LayoutFactory::getLayout(LayoutFactory::TEMP_ONE_COl)
                 ->setCss(array(
                     '/style/details.css',
@@ -54,43 +58,8 @@ class product extends BaseController
                     '/js/controller/productDetailsCtrl.js',
                     '/plugins/jqzoom_ev-2.3/js/jquery.jqzoom-core.js'
                 ))
-                ->setBreadcrums($breadcrums)
                 ->setData($data, true)
                 ->render('product/details');
-    }
-
-    function related_products_service($productID)
-    {
-        header('Content-type:application/json');
-        $this->load->model('modelEx/ProductModel', 'productModel');
-        $this->productModel->updateCountView(User::getCurrentUser()->id, $productID);
-
-        $mapper = ProductFixedMapper::make()
-                ->select('p.*', true)
-                ->selectCountView()
-                ->autoloadAttributes()
-                ->setLanguage(User::getCurrentUser()->languageKey)
-                ->filterRelatedProduct($productID);
-        $mapper->getQuery()
-                ->select('seller.name AS seller_name')
-                ->innerJoin('t_seller seller', 'p.fk_seller = seller.id')
-                ->limit(9);
-        $products = $mapper->findAll(function($record, ProductFixedDomain $instance)
-        {
-            $instance->seller_name = $record['seller_name'];
-        });
-        $json = array();
-        foreach ($products as $product)
-        {
-            $images = $product->getImages('thumbnail');
-            $obj = get_object_vars($product);
-            $obj['name'] = strval($product->getName());
-            $obj['thumbnail'] = $images ? strval($images[0]->url) : '';
-            $obj['priceString'] = strval($product->getPriceMoney(User::getCurrentUser()->getCurrency()));
-            $obj['url'] = base_url('product/details') . '/' . $product->id;
-            $json[] = $obj;
-        }
-        echo json_encode($json);
     }
 
 }
