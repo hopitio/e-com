049048199fe7edbedf50dadbfa67bc1c9ac881df
ANLT <lethanhan.bkaptech@gmail.com>
2015-01-13 21:39:40 +0700

    debug code

--- src/application/core/MY_Controller.php
+++ src/application/core/MY_Controller.php
@@ -100,6 +100,7 @@ class MY_Controller extends CI_Controller
             {
                 throw new Lynx_RoutingException();
             }
+            var_dump($this->_controller); die;
             call_user_func_array(array($this, $method), $ar_arg);
         }
         catch (Lynx_ViewException $e)
